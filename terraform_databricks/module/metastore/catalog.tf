af2dee072f2e2e1f8c5d47d04f4ffbf0212c4383
RenukaPrasad <99643101+RnukaPrasad@users.noreply.github.com>
2024-02-13 16:58:10 +0530

    Create catalog.tf

---
+++ terraform_databricks/module/metastore/catalog.tf
@@ -0,0 +1,6 @@
+# for creating catalog
+resource "databricks_catalog" "catalog" {
+  depends_on = [ databricks_metastore_assignment.default_metastore ]
+  metastore_id = databricks_metastore.metastore.id
+  name         = var.catalog_name
+}
