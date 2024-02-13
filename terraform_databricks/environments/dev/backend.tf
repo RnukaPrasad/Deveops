0d6d8a9002cd2b82467e1e8c959ce0d6b46c404f
RenukaPrasad <99643101+RnukaPrasad@users.noreply.github.com>
2024-02-13 16:51:30 +0530

    Create backend.tf

---
+++ terraform_databricks/environments/dev/backend.tf
@@ -0,0 +1,10 @@
+# #terraform {
+#     backend "azurerm" {
+#         resource_group_name  = "workingtest"
+#         storage_account_name = "terrfilestate"
+#         container_name       = "terraform-state"
+#         key                  = "dev.tfstate"
+#         access_key           = "oLNK4V/aX372hpqWhQPHY+LwS+wGwe1ez0w2du5aBM+/sTh5K1O0m0cOyzHpU89j1qznJIIQe6GW+AStss54+Q=="
+#     }
+
+# }
